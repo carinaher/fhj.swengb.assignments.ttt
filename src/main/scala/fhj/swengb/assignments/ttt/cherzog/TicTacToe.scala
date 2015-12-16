20644a1658caa23a80d8947feb26d75f5a10e2d7
carinaher <carina.herzog@gmail.com>
2015-12-16 14:57:19 +0100

    improved asString function

--- src/main/scala/fhj/swengb/assignments/ttt/cherzog/TicTacToe.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/cherzog/TicTacToe.scala
@@ -124,7 +124,21 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     *
     * @return
     */
-  def asString(): String = ???
+  def asString(): String = {
+
+    val seperator = "|---|---|---|\n"
+    val board = List(TopLeft.idx,TopCenter.idx,TopRight.idx,
+      MiddleLeft.idx,MiddleCenter.idx,MiddleRight.idx,
+      BottomLeft.idx,BottomCenter.idx,BottomRight.idx)
+
+    seperator +
+      "|-"+board(0)+"-|-"+board(1)+"-|-"+board(2)+"-|\n"+
+      seperator +
+      "|-"+board(3)+"-|-"+board(4)+"-|-"+board(5)+"-|\n"+
+      seperator +
+      "|-"+board(6)+"-|-"+board(7)+"-|-"+board(8)+"-|\n"+
+      seperator)
+}
 
 
 
